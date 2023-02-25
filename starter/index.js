0de0f789ffe9732c9aec90be24ed7dc4e78b22bb
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2023-02-25 17:51:58 +0000

    deleted completed psudo code

--- starter/index.js
+++ starter/index.js
@@ -162,58 +162,23 @@ const internPrompt = () => {
         
         selectTeamMemberPrompt();
            
-    })
-}
+    });
+};
 
 
 function renderPage () {
     console.log("woo");
     console.log(TeamMemberArray); 
-    
-}
+
+};
 
 // const buildPage = () => {
 // // render(ArrayOfTeamMembers)
 // }
 // // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-// inquirer.prompt([{
-//     //manager questions
-// }]).then(response => {
-//     // populate manager info
-//     // promptForNexEmployee ()
-// })
 
-// const promptForNextEmployee = () => {
-//     inquirer.prompt([{
-//         // choice of 3
-//     }]).then(response => {
-//         // if engineer
-//         //    promptForEngineer
-//         // else if intern
-//         //    promptForIntern
-//         // else
-//         //    use the functionality from page-template to generate the team
-//     })
-// }
 
-// const promptForEngineer = () => {
-//     inquirer.prompt([{
-//         //engineer questions
-//     }]).then(response => {
-//         // add new engineer to employees array
-//         // promptForNextEmployee
-//     })
-// }
-
-// const promptForIntern = () => {
-//     inquirer.prompt([{
-//         //intern questions
-//     }]).then(response => {
-//         // add new intern to employees array
-//         // promptForNextEmployee
-//     })
-// }
 
 // const buildPage = () => {
 //     const testArray = []
